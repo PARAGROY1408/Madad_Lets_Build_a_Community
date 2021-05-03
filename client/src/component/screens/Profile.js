e7a99ee2d48313ccf20321040cb4d1c4e844a4b7
PARAG ROY <43987702+PARAGROY1408@users.noreply.github.com>
2021-05-03 19:56:06 +0530

    Update Profile.js

--- client/src/component/screens/Profile.js
+++ client/src/component/screens/Profile.js
@@ -1,21 +1,20 @@
 import React, { useEffect, useState, useContext } from 'react'
 import { UserContext } from '../../App'
 const Profile = () => {
-    // we have to use the useEffect hook bcz we need to get the data from the backend...
-    // we need all the post that is made by that particuar user........
+    
     const [mypics, setPics] = useState([])
-    // mypics is an array and and it will contain all the posts made by that particular user..
+    
 
-    const [readMore, setReadMore] = useState(false); //used to implement read more functionality..
+    const [readMore, setReadMore] = useState(false); 
     const linkName = readMore ? 'Read Less << ' : 'Read More >> '
 
 
     const { state, dispatch } = useContext(UserContext)
 
     const [image, setImage] = useState("")
-    //const [url, setUrl] = useState("")
+    
     useEffect(() => {
-        // in this we will make a network request to the backend by fetch..
+        
         fetch('/mypost', {
             headers: {
                 "Authorization": "Bearer " + localStorage.getItem("jwt")
@@ -24,10 +23,9 @@ const Profile = () => {
             .then(res => res.json())
             .then(result => {
                 setPics(result.mypost)
-                // now mypics is an array of js objects and we need to fetch only the image from it..
-                // with the help of photo key..
+                
             })
-    }, []) // want the useEffect to get fired only once..
+    }, []) 
 
     useEffect(() => {
         if (image) {
@@ -36,24 +34,20 @@ const Profile = () => {
             data.append("upload_preset", "insta-clone")
             data.append("cloud_name", "paragpramodroy")
 
-            // as this fetch is of post so we have to pass the method inside it...
+            
             fetch("	https://api.cloudinary.com/v1_1/paragpramodroy/image/upload", {
                 method: "post",
                 body: data
             })
-                .then(res => res.json()) // we will get the response then convert it to the json format..
+                .then(res => res.json()) 
                 .then(data => {
                     /*
                     console.log(data)
-                    isse console karke dekha data eik js ka object hai..it have different key value pair..
-                    usme se eik key hai url ki we want so we will use it like data.url
+                   
                     */
                     //setUrl(data.url)
                     console.log(data)
-                    // we need to update our state...
-                    // then we need to make changes in the localstoarge also..
-                    //localStorage.setItem("user",JSON.stringify({...state,pic:data.url}))
-                    //dispatch({type:"UPDATEPIC",payload:data.url})
+                    
 
                     fetch('/updatepic', {
                         method: "put",
@@ -74,12 +68,7 @@ const Profile = () => {
                         .catch(err => {
                             console.log(err)
                         })
-                    // now from here we will make a network request to the backend..
-                    // at the backend we have defined a updatepic route and we will send the pic given by the user.
-                    /*
-                    //window.location.reload() //we are getting some error..
-                    //length is not defined so inorder to get rid of that..
-                    // we will reload the page after updatring the pic*/
+                    
                 })
                 .catch(err => {
                     console.log(err)
@@ -87,10 +76,9 @@ const Profile = () => {
         }
     }, [image])
 
-    const updatePhoto = (file) => { // we will receive a file 
-        /* we have to post the image to the cloud*/
+    const updatePhoto = (file) => { 
 
-        setImage(file) // we passed the file in this way alsoe..
+        setImage(file) 
 
     }
     const x = readMore ? 'Read Less.. ' : 'Read More.. '
